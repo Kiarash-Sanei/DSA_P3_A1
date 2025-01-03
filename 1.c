5203b7dc7d31ef94eb853dcc82218d809a6d0baa
Kiarash Sanei <kiarashsanei5@gmail.com>
2025-01-03 08:36:22 +0330

    First attempt is working ok but not optimized

--- 1.c
+++ 1.c
@@ -1,50 +1,77 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct node {
-    int* nieghbor;
+typedef struct node
+{
+    int *nieghbor;
     int size;
     int shortest;
-    int times;
+    int path;
 } node;
 
-void dfs(node* nodes, int index, int trail, int n) 
+void bfs(node *nodes, int index, int trail, int n)
 {
     int shortest = nodes[index].shortest + 1;
+    trail++;
+    if (trail == n)
+    {
+        return;
+    }
     for (int i = 0; i < nodes[index].size; i++)
     {
-        node* current = nodes + nodes[index].nieghbor[i];
+        node *current = nodes + nodes[index].nieghbor[i];
         if (shortest < current->shortest)
         {
             current->shortest = shortest;
-            current->times = 1;
-            dfs(nodes, current - nodes, trail + 1, n);
         }
-        else if (shortest = current->shortest && trail <= n )
-        {
-            current->times += 1;
-            dfs(nodes, current - nodes, trail + 1, n);
-        }
+    }
+    for (int i = 0; i < nodes[index].size; i++)
+    {
+        node *current = nodes + nodes[index].nieghbor[i];
+        bfs(nodes, current - nodes, trail, n);
     }
 }
 
-int main() 
+void pathCounter(node *nodes, int index, int trail, int n)
+{
+    trail++;
+    if (trail == n)
+    {
+        return;
+    }
+    for (int i = 0; i < nodes[index].size; i++)
+    {
+        node *current = nodes + nodes[index].nieghbor[i];
+        if (trail == current->shortest)
+        {
+            current->path++;
+        }
+    }
+    for (int i = 0; i < nodes[index].size; i++)
+    {
+        node *current = nodes + nodes[index].nieghbor[i];
+        pathCounter(nodes, current - nodes, trail, n);
+    }
+}
+
+int main()
 {
     int n, m;
     scanf("%d %d", &n, &m);
     node nodes[n];
     for (int i = 0; i < n; i++)
     {
-        nodes[i].nieghbor = (int*)malloc(m * sizeof(int));
+        nodes[i].nieghbor = (int *)malloc(m * sizeof(int));
         nodes[i].size = 0;
         nodes[i].shortest = __INT32_MAX__;
-        nodes[i].times = 0;
+        nodes[i].path = 0;
     }
     nodes[0].shortest = 0;
+    nodes[0].path = 1;
 
     for (int i = 0; i < m; i++)
     {
-        int a,b;
+        int a, b;
         scanf("%d %d", &a, &b);
         a--;
         b--;
@@ -54,9 +81,10 @@ int main()
         nodes[b].size++;
     }
 
-    dfs(nodes, 0, 1, n);
+    bfs(nodes, 0, 0, n);
+    pathCounter(nodes, 0, 0, n);
 
-    printf("%d\n", nodes[n - 1].times);
-    
+    printf("%d\n", nodes[n - 1].path);
+
     return 0;
 }
